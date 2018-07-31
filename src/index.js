e18881bd2c53f9dc9e3c7a2a47a4baf95e4557b0
Shriney Malekar <shriney.malekar.gmail@gmail.com>
2018-07-31 08:07:45 +0530

    Middleware added

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import reducer from './store/Reducer';
 import { Provider } from 'react-redux';
 
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from './registerServiceWorker';\
+
+// Middleware
+const logger = store => {
+    return next => {
+        return action => {
+            const result = next(action);
+            return result;
+        }
+    }
+}
 
 // Store
-const store = createStore(reducer);
+const store = createStore(reducer, applyMiddleware(logger));
+// here applyMiddleware(logger) in the above createStore() function called Enhancer
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
